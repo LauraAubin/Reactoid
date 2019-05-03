c75b7e1d811ac04c20f920043339c0688d75ea35
Laura Aubin <laura.aubin@shopify.com>
2019-05-03 00:38:21 -0400

    Create option glow and adjust spacings

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -3,14 +3,14 @@ import * as React from 'react';
 import { DisplayText } from '@shopify/polaris';
 
 import Icon from './components/Icon';
-import Heading from "../Typography/Heading";
+import Heading from '../Typography/Heading';
 
 import './Home.scss';
 
 export default class Home extends React.Component {
   public render() {
-    const purple = "#6052b2";
-    const yellow = "#ffe787";
+    const purple = '#6052b2';
+    const yellow = '#ffe787';
 
     return (
       <div className='Layout'>
@@ -19,11 +19,15 @@ export default class Home extends React.Component {
         </DisplayText>
         <div className='OptionsContainer'>
           <div className='Option'>
-            <Icon name='folder' size='medium' color={purple} />
+            <div className='IconBottomPadding'>
+              <Icon name='folder' size='medium' color={purple} />
+            </div>
             <Heading color={purple}>Custom</Heading>
           </div>
           <div className='Option'>
-            <Icon name='penguin' size='medium' color={yellow} />
+            <div className='IconBottomPadding'>
+              <Icon name='penguin' size='medium' color={yellow} />
+            </div>
             <Heading color={yellow}>Use existing</Heading>
           </div>
         </div>
