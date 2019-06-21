dd23810ebf16a38a00171a0d62441e92e26c4ec8
Laura Aubin <laura.aubin@shopify.com>
2019-06-21 16:08:56 -0400

    Create a Stack component that takes distribution and alignment as props

---
+++ src/components/Stack/Stack.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import classNames from 'classnames';
+
+import './Stack.scss';
+
+interface Props {
+  distribution?: 'center' | 'trailing';
+  alignment?: 'center';
+  children: any;
+}
+
+export default function Stack({ distribution, alignment, children }: Props) {
+  const classes = classNames(
+    'Stack',
+    distribution && Distribution(distribution),
+    alignment && Alignment(alignment)
+  );
+
+  return <div className={classes}>{children}</div>;
+}
+
+function Distribution(distribution: Props['distribution']) {
+  if (distribution == 'trailing') return 'HorizontalFlexEnd';
+  if (distribution == 'center') return 'HorizontalCenter';
+}
+
+function Alignment(alignment: Props['alignment']) {
+  if (alignment == 'center') return 'VerticalCenter';
+}
